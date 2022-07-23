1ba015fe05a32b892f20e894881c82b0d0adb92f
Br00ty <aribant92@gmail.com>
2022-07-23 04:25:17 -0400

    More misc. updates

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -3,6 +3,7 @@ ITEM_MAPPING = {
     [100275] = {"basin", "toggle"},
     [100266] = {"doll", "toggle"},
     [100292] = {"dragon", "toggle"},
+    [100294] = {"storm", "toggle"},
     [100286] = {"jailbreaker", "toggle"},
     [100291] = {"jailring", "toggle"},
     [100290] = {"oldcell", "toggle"},
